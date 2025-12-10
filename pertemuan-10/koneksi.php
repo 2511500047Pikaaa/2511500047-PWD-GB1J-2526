e4e557ac99a3c394d0bc2601b925512ba5ec34f4
Fika Haliza <2511500047@mahasiswa.atmaluhur.ac.id>
2025-12-10 03:06:35 -0800

    koneksi.php

--- pertemuan-10/koneksi.php
+++ pertemuan-10/koneksi.php
@@ -4,7 +4,8 @@ $user = "root";
 $pass = "";
 $db   = "db_pwd2025";
 
-$conn = mysql_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-  die("koneksi gagal: " . mysqli_connect_error())}
+  die("Koneksi gagal: " . mysqli_connect_error());
+}
