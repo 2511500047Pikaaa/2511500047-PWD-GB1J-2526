186dbe242cee6603765eef2cf207c7b5845d563e
Fika Haliza <2511500047@mahasiswa.atmaluhur.ac.id>
2026-01-22 17:10:30 +0700

    hasil record yang di update ditampilkan pada file pembaca

--- pertemuan-16/koneksi.php
+++ pertemuan-16/koneksi.php
@@ -1,11 +1,6 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_pwd2025";
+$conn = new mysqli("localhost", "root", "", "biodata_pengunjung");
 
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-  die("Koneksi gagal: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
